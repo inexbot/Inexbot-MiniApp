695302557c216f2d4fb249612cb0d6762b225c56
weifan <weifan@inexbot.com>
2020-07-21 18:24:46 +0800

    init project

---
+++ src/pages/index/index.jsx
@@ -0,0 +1,171 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, Swiper, SwiperItem, ScrollView } from "@tarojs/components";
+import { AtButton, AtSearchBar, AtGrid, AtListItem, AtList } from "taro-ui";
+import "./index.less";
+import { requestNews, requestVideo, requestDocument } from "../../request/api";
+import Taro from "@tarojs/taro";
+
+export default function Index(props) {
+  const [searchBarValue, setSearchBarValue] = useState("");
+  const [news1, setNews1] = useState(<View className="index-news">新闻1</View>);
+  const [news2, setNews2] = useState(<View className="index-news">新闻2</View>);
+  const [news3, setNews3] = useState(<View className="index-news">新闻3</View>);
+  const [videoList, setVideoList] = useState(
+    <View className="index-video-list">小课堂视频</View>
+  );
+  const [documentList, setDocumentList] = useState(
+    <View className="index-document-list">手册</View>
+  );
+  const changeSearchBar = (value) => {
+    setSearchBarValue(value);
+  };
+  const clickButton = (item, index) => {
+    return;
+  };
+  const clickSwiper = (aid) => {
+    console.log(aid);
+  };
+  const clickDocument = (url) => {
+    Taro.downloadFile({
+      url: url,
+      success: function (res) {
+        var filePath = res.tempFilePath;
+        Taro.openDocument({
+          filePath: filePath,
+          success: function (res) {
+            console.log("打开文档成功");
+          },
+        });
+      },
+    });
+  };
+  const clickVideo = (url) => {
+    Taro.navigateToMiniProgram({
+      appId: "wx7564fd5313d24844",
+    });
+  };
+  useEffect(() => {
+    async function fechData() {
+      let NewsRes = await requestNews(1, 3);
+      let newslist = NewsRes.data.data.list;
+      newslist.forEach((value, index, array) => {
+        // 得到value.title(string),value.litpic(url-string)
+        let newsli = (
+          <View
+            className="index-news"
+            style={{ backgroundImage: value.litpic }}
+            onClick={clickSwiper.bind(this, value.id)}
+          >
+            {value.title}
+          </View>
+        );
+        if (index === 0) {
+          setNews1(newsli);
+        } else if (index === 1) {
+          setNews2(newsli);
+        } else if (index === 2) {
+          setNews3(newsli);
+        }
+      });
+    }
+    fechData();
+  }, []);
+  useEffect(() => {
+    async function fechData() {
+      let videoRes = await requestVideo(1, 4);
+      let videoList = videoRes.data.data.list;
+      let listitem = [];
+      let kk = 0;
+      videoList.forEach((value, index, array) => {
+        // 得到value.name,value.link,value.chap,value.sort,value.frameurl
+        let newv = (
+          <View
+            className="index-video-context"
+            onClick={clickVideo.bind(this, value.link)}
+            key={kk}
+          >
+            <img src="https://forinexbotweb.oss-cn-shanghai.aliyuncs.com/uploads/20200601/%E5%B0%8F%E8%AF%BE%E5%A0%82-1.png" />
+            <Text>{value.name}</Text>
+          </View>
+        );
+        listitem.push(newv);
+        kk++;
+      });
+      setVideoList(listitem);
+    }
+    fechData();
+  }, []);
+  useEffect(() => {
+    async function fechData() {
+      let documentRes = await requestDocument(1, 4);
+      let documentList = documentRes.data.data.list;
+      let listitem = [];
+      let kk = 0;
+      documentList.forEach((value, index, array) => {
+        // 得到value.aid,value.name,value.link,value.sort,value.version
+        listitem.push(
+          <AtListItem
+            title={value.name}
+            onClick={clickDocument.bind(this, value.link)}
+            note={`版本${value.version}`}
+            key={kk}
+          />
+        );
+        kk++;
+      });
+      let list = <AtList>{listitem}</AtList>;
+      setDocumentList(list);
+    }
+    fechData();
+  }, []);
+  return (
+    <View className="index">
+      <AtSearchBar
+        showActionButton
+        value={searchBarValue}
+        onChange={changeSearchBar}
+      />
+      <Swiper
+        className="test-h"
+        indicatorColor="#999"
+        indicatorActiveColor="#333"
+        circular={true}
+        indicatorDots
+        autoplay
+        interval={3000}
+      >
+        <SwiperItem>{news1}</SwiperItem>
+        <SwiperItem>{news2}</SwiperItem>
+        <SwiperItem>{news3}</SwiperItem>
+      </Swiper>
+      <AtGrid
+        columnNum={4}
+        data={[
+          {
+            image:
+              "https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png",
+            value: "小课堂",
+          },
+          {
+            image:
+              "https://img20.360buyimg.com/jdphoto/s72x72_jfs/t15151/308/1012305375/2300/536ee6ef/5a411466N040a074b.png",
+            value: "文档",
+          },
+          {
+            image:
+              "https://img10.360buyimg.com/jdphoto/s72x72_jfs/t5872/209/5240187906/2872/8fa98cd/595c3b2aN4155b931.png",
+            value: "新闻",
+          },
+          {
+            image:
+              "https://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png",
+            value: "纳博特",
+          },
+        ]}
+        onClick={clickButton}
+      />
+      <ScrollView className="index-videos">{videoList}</ScrollView>
+      <View className="index-document">{documentList}</View>
+    </View>
+  );
+}
---
+++ src/request/api.js
@@ -0,0 +1,88 @@
+import Taro from "@tarojs/taro";
+
+const url = "https://hd215.api.yesapi.cn/";
+const app_key = "A9B8F37512C199D5FE1BDC229CD9E36C";
+
+export function requestNews(page, perpage) {
+  let where = '[["typeid", "=", "8"]]';
+  let order = '["weight DESC"]';
+  const res = Taro.request({
+    url: url,
+    method: "POST",
+    header: { "Content-Type": "application/x-www-form-urlencoded" },
+    data: {
+      s: "App.SuperTable.FreeQuery",
+      app_key: app_key,
+      model_name: "dede_archives",
+      where: where,
+      order: order,
+      page: page,
+      perpage: perpage,
+    },
+  });
+  return res;
+}
+
+export function requestNewsBody(aid) {
+  let where = JSON.stringify([["aid", "=", aid]]);
+  let order = '["weight DESC"]';
+  Taro.request({
+    url: url,
+    method: "POST",
+    header: { "Content-Type": "application/x-www-form-urlencoded" },
+    data: {
+      s: "App.SuperTable.FreeQuery",
+      app_key: app_key,
+      model_name: "dede_archives",
+      where: where,
+      order: order,
+      page: page,
+      perpage: perpage,
+    },
+    success: (res) => {
+      return res;
+    },
+    fail: (res) => {
+      return res;
+    },
+  });
+}
+
+export function requestDocument(page, perpage) {
+  let where = '[["typeid", "=", "58"]]';
+  let order = '["sort ASC"]';
+  const res = Taro.request({
+    url: url,
+    method: "POST",
+    header: { "Content-Type": "application/x-www-form-urlencoded" },
+    data: {
+      s: "App.SuperTable.FreeQuery",
+      app_key: app_key,
+      model_name: "dede_allmanual",
+      where: where,
+      order: order,
+      page: page,
+      perpage: perpage,
+    },
+  });
+  return res;
+}
+export function requestVideo(page, perpage) {
+  let where = '[["typeid", "=", "60"]]';
+  let order = '["sort ASC"]';
+  const res = Taro.request({
+    url: url,
+    method: "POST",
+    header: { "Content-Type": "application/x-www-form-urlencoded" },
+    data: {
+      s: "App.SuperTable.FreeQuery",
+      app_key: app_key,
+      model_name: "dede_teachclass",
+      where: where,
+      order: order,
+      page: page,
+      perpage: perpage,
+    },
+  });
+  return res;
+}
